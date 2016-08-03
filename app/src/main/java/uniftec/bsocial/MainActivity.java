6b3490cb68ebafdbd123a8e3dac2c87a6beee728
manfrowar <mauriciomanfro@gmail.com>
2016-08-03 19:09:44 -0300

    Facebook SDK initial integration

--- app/src/main/java/uniftec/bsocial/MainActivity.java
+++ app/src/main/java/uniftec/bsocial/MainActivity.java
@@ -2,12 +2,16 @@ package uniftec.bsocial;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(getApplication());
         setContentView(R.layout.activity_main);
     }
 }
