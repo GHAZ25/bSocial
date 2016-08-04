eb116f1359fb17b502b03fa0cbce690bd18ad29b
GHAZ25 <ghaz2.5@gmail.com>
2016-08-04 08:11:15 -0300

    Botão de acesso ao Facebook na tela inicial e declaração das funções de retorno

--- app/src/main/java/uniftec/bsocial/MainActivity.java
+++ app/src/main/java/uniftec/bsocial/MainActivity.java
@@ -1,17 +1,50 @@
 package uniftec.bsocial;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
+    CallbackManager callbackManager = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
         FacebookSdk.sdkInitialize(getApplicationContext());
         AppEventsLogger.activateApp(getApplication());
-        setContentView(R.layout.activity_main);
+        //callbackManager = CallbackManager.Factory.create();
+        callbackManager = new CallbackManager() {
+            @Override
+            public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
+                return false;
+            }
+        };
+
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            } });
     }
 }
