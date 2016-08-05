25d277fae6854f267be2c471113eccd40b10df83
manfrowar <mauriciomanfro@gmail.com>
2016-08-04 21:40:35 -0300

    Facebook SDK integration Facebook login initial integration

--- app/src/main/java/uniftec/bsocial/MainActivity.java
+++ app/src/main/java/uniftec/bsocial/MainActivity.java
@@ -1,36 +1,45 @@
 package uniftec.bsocial;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telecom.Call;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import org.json.JSONException;
+
 public class MainActivity extends AppCompatActivity {
-    CallbackManager callbackManager = null;
+    public static CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         FacebookSdk.sdkInitialize(getApplicationContext());
+
+        setContentView(R.layout.activity_main);
+
         AppEventsLogger.activateApp(getApplication());
-        //callbackManager = CallbackManager.Factory.create();
-        callbackManager = new CallbackManager() {
-            @Override
-            public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
-                return false;
-            }
-        };
+        callbackManager = CallbackManager.Factory.create();
 
         LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("public_profile", "email");
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
