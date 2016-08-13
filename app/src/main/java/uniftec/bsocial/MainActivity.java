a9546071bdc468d89418db0acb6bcd150d4a79cc
manfrowar <mauriciomanfro@gmail.com>
2016-08-13 01:29:14 -0300

    Telas pt3

--- app/src/main/java/uniftec/bsocial/MainActivity.java
+++ app/src/main/java/uniftec/bsocial/MainActivity.java
@@ -1,30 +1,17 @@
 package uniftec.bsocial;
 
-import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.telecom.Call;
-import android.util.AttributeSet;
-import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
-import com.facebook.AccessToken;
-import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
-import com.facebook.GraphRequest;
-import com.facebook.GraphResponse;
-import com.facebook.HttpMethod;
 import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-
-import org.json.JSONException;
 
 public class MainActivity extends AppCompatActivity {
     public static CallbackManager callbackManager;
